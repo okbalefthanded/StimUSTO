5db22771f7d18a8f17c9091a8eb54d8ac7a2a1e9
unknown <okba.bekhelifi@gmail.com>
2021-02-02 09:34:00 +0100

    - changing stim order in multistim

--- include/multistimuli.h
+++ include/multistimuli.h
@@ -19,6 +19,7 @@ protected slots:
 
 private:
     QList<QPixmap> m_stimuli;
+
     QMap<int, QString> m_stimuliMap{{1, "bennabi_face_red_inverted.png"},
                                     {2, "yin_yang_small_croped.png"},
                                     {3, "whitehouse_small.png"},
@@ -29,7 +30,17 @@ private:
                                     {8, "yin_yang_small_croped.png"},
                                     {9, "whitehouse_small.png"}
                                    };
-
+    /*
+    QMap<int, QString> m_stimuliMap{{1, "bennabi_face_red_inverted.png"},
+                                    {2, "whitehouse_small.png"},
+                                    {3, "yin_yang_small_croped.png"},
+                                    {4, "bennabi_face_blue_inverted.png"},
+                                    {5, "whitehouse_small.png"},
+                                    {6, "yin_yang_small_croped.png"},
+                                    {7, "bennabi_face_magenta_inverted.png"},
+                                    {8, "whitehouse_small.png"},
+                                    {9, "yin_yang_small_croped.png"},
+                                   };*/
 };
 
 #endif // MULTISTIMULI_H
